535ecf0f13807694e33f396de85d8d97376d0d09
ForgottenZ <51231903+ForgottenZ@users.noreply.github.com>
2026-02-15 13:15:23 +0800

    feat(i18n): add zh-CN localization and default Chinese UI

--- alist-backend/internal/bootstrap/data/setting.go
+++ alist-backend/internal/bootstrap/data/setting.go
@@ -165,7 +165,7 @@ func InitialSettings() []model.SettingItem {
 		{Key: conf.Token, Value: token, Type: conf.TypeString, Group: model.SINGLE, Flag: model.PRIVATE},
 		{Key: conf.DefaultTryRapidUpload, Value: "false", Type: conf.TypeBool, Group: model.SINGLE, Flag: model.PUBLIC},
 		{Key: conf.DefaultChunkUploadEnabled, Value: "false", Type: conf.TypeBool, Group: model.SINGLE, Flag: model.PUBLIC},
-		{Key: conf.FooterText, Value: "Powered by AList", Type: conf.TypeString, Group: model.SINGLE, Flag: model.PUBLIC},
+		{Key: conf.FooterText, Value: "由 AList 驱动", Type: conf.TypeString, Group: model.SINGLE, Flag: model.PUBLIC},
 		{Key: conf.SearchIndex, Value: "none", Type: conf.TypeSelect, Options: "database,database_non_full_text,bleve,meilisearch,none", Group: model.INDEX},
 		{Key: conf.AutoUpdateIndex, Value: "false", Type: conf.TypeBool, Group: model.INDEX},
 		{Key: conf.IgnorePaths, Value: "", Type: conf.TypeText, Group: model.INDEX, Flag: model.PRIVATE, Help: `one path per line`},
